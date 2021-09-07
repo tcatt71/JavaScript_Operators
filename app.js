1cffc557704fec8479ca5b5d3adc3c735ba6f409
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-09-06 22:06:11 -0700

    complete exercise one

--- app.js
+++ app.js
@@ -6,7 +6,31 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+/* ## Exercise 1
+
+1. Declare 2 variables, `a` and `b`, and assign 20 to `a` and 4 to `b`
+2. Declare a variable `add` that uses the `+` operator to store the result of adding the values stored in `a` and `b`
+3. Declare a variable `minus` that uses the `-` operator to store the result of subtracting the values stored in `a` and `b`
+4. Declare a variable `multiply` that uses the `*` operator to store the result of multiplying the values stored in `a` and `b`
+5. Declare a variable `dividing` that uses the `/` operator to store the result of dividing the values stored in `a` and `b`
+
+You can print the value of the variables to the browser console (ex: `console.log(add)`) to check the result. */
+
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log('a');
+console.log('b');
+console.log('add');
+console.log('minus');
+console.log('multiply');
+console.log('dividing');
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
