0f4a15d6dcc9904568ebbe99cfa3dde3529612b8
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-09-06 22:29:46 -0700

    complete exercises two and three

--- app.js
+++ app.js
@@ -17,27 +17,84 @@ console.log("EXERCISE 1:\n==========\n");
 You can print the value of the variables to the browser console (ex: `console.log(add)`) to check the result. */
 
 // YOUR CODE HERE
-let a = 20;
-let b = 4;
-let add = a + b;
-let minus = a - b;
-let multiply = a * b;
-let dividing = a / b;
+// let a = 20;
+// let b = 4;
+// let add = a + b;
+// let minus = a - b;
+// let multiply = a * b;
+// let dividing = a / b;
 
-console.log('a');
-console.log('b');
-console.log('add');
-console.log('minus');
-console.log('multiply');
-console.log('dividing');
+// console.log('a');
+// console.log('b');
+// console.log('add');
+// console.log('minus');
+// console.log('multiply');
+// console.log('dividing');
 
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
+/* 1. Use the following code to answer the questions below:
+
+``` */
+// let a = 11;
+// let str = "11";
+// let str2 = "eleven";
+// let isPresent = true;
+// let firstName = "Jackie";
+// let lastName = "Chan";
+/* ```
+
+- What is the value of: a + str?  // "1111"
+- What is the value of: a + str2?  // "11eleven"
+- What is the value of: a + isPresent?  // Nan
+- What is the value of: a + firstName?  // "11Jackie"
+- What is the value of: a + lastName?  // "11Chan"
+
+Use the code above to test and print the results. */
+
 // YOUR CODE HERE
+// console.log(a + str);
+// console.log(a + str2);
+// console.log(a + isPresent);
+// console.log(a + firstName);
+// console.log(a + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
+/* 1. Use the following code to answer the questions below:
+
+``` */
+let a = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+let firstName = "Robin";
+let lastName = "Williams";
+/* ```
+
+- What is the value of: a == str?  // true
+- What is the value of: a === str?  // false
+- What is the value of: !isPresent?  // true
+- What is the value of: (“eleven” == str2 && a >= str)?  // false
+- What is the value of: (!isPresent || isPresent)?  // true
+- What is the value of: 0 == false?  // true
+- What is the value of: 0 === false?  // false
+- What is the value of: 0 != false?  // false
+- What is the value of 0 !== false?  // true
+
+Use the code above to test and print the results. */
+
 // YOUR CODE HERE
+
+console.log(a == str);
+console.log(a === str);
+console.log(!isPresent);
+console.log('eleven' == str2 && a >= str);
+console.log(!isPresent || isPresent);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log( 0 !== false);
